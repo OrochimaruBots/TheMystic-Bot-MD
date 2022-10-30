ae8a9b041018cf60581b0f1758bf799e26c8ab5c
OrochimaruBotz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-10-30 18:46:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50575008120', '🤖 Mystic - Bot 🤖', true],
-  ['17722386341']
+  ['6288804947282', '👑 Mystic - Creador 👑', true],
+  ['6288804947282', '🤖 Mystic - Bot 🤖', true],
+  ['6288804947282']
 ] // Cambia los numero que quieras
 global.mods = [] 
 global.prems = [] 
